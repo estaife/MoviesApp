926f863bf557622056e635eb80fb9eb22f25dfd4
Estaife Lima <estaife@icloud.com>
2020-08-30 02:18:40 -0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Estaife Lima on 30/08/20.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
