027caf59bc499553e2281e7f4c303da4ca385904
Estaife Lima <idd_elima@uolinc.com>
2021-03-01 23:45:13 -0400

    Testing testGetAllPopularMoviesWithSuccess

--- DataTests/PopularMoviesUseCaseTests.swift
+++ DataTests/PopularMoviesUseCaseTests.swift
@@ -17,23 +17,54 @@ class PopularMoviesUseCaseTests: XCTestCase {
         let (sut, httpGetClientSpy) = createSut(url: url)
         
         //When
-        sut.getAllPopularMovies(page: "1") { _ in }
-        httpGetClientSpy.completeWith(data: movieResultsData)
+        sut.getAllPopularMovies(page: page) { _ in }
+        httpGetClientSpy.completeWith(data: movieResultsSuccess.data!)
         
         //Then
         XCTAssertEqual(httpGetClientSpy.url.first, url)
     }
+    
+    func testGetAllPopularMoviesWithSuccess() {
+        
+        //Given
+        let (sut, httpGetClientSpy) = createSut(url: url)
+        let expectedResult: Result<MovieResults, DomainError> = .success(movieResultsSuccess)
+        
+        //When
+        let expect = expectation(description: "Waiting for popular movies")
+        sut.getAllPopularMovies(page: page) { receivedResult in
+            switch (expectedResult, receivedResult) {
+            case (.failure(let expectedError), .failure(let receivedError)):
+                XCTAssertEqual(expectedError, receivedError)
+            case (.success(let expectedModel), .success(let receivedModel)):
+                XCTAssertEqual(expectedModel, receivedModel)
+                XCTAssertEqual(expectedModel.page, receivedModel.page)
+                XCTAssertEqual(expectedModel.totalPages, receivedModel.totalPages)
+                XCTAssertEqual(expectedModel.results, receivedModel.results)
+            default: XCTFail("Fail, because expected and received not equal")
+            }
+            expect.fulfill()
+        }
+        httpGetClientSpy.completeWith(data: movieResultsSuccess.data!)
+        
+        //Then
+        wait(for: [expect], timeout: 1)
+    }
 }
 
 extension PopularMoviesUseCaseTests {
-    var movieResultsData: Data {
+    var movieResultsSuccess: MovieResults {
         let simpleMovieResponse = SimpleMovieResponse(posterPath: nil, releaseDate: "2021-01-01", identifier: 1, title: "Movie Test", popularity: 0.8)
         let movieResults = MovieResults(page: 1, results: [simpleMovieResponse], totalPages: 10, totalResults: 1)
-        return movieResults.data!
+        return movieResults
     }
     
     var url: URL {
         URL(string: "https://test.com")!
+    }
+    
+    var page: String {
+        "0"
     }
     
     func createSut(url: URL) -> (sut: PopularMoviesUseCase, httpGetClientSpy: HTTPGetClientSpy) {
