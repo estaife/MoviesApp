a26f9b1af42976a240455f323a9ffb62c8792d99
Estaife Lima <estaife@icloud.com>
2021-10-16 17:36:21 -0300

    Adjusting some name of class into PresenterTests

---
+++ PresenterTests/Scenes/PopularMoviesPresenterTests.swift
@@ -0,0 +1,11 @@
+//
+//  PresenterTests.swift
+//  PresenterTests
+//
+//  Created by Estaife Lima on 11/10/21.
+//
+
+import XCTest
+@testable import Presenter
+
+class PopularMoviesPresenterTests: XCTestCase { }
