60398a134f51db8e1d5a22645002d91ef00ac68e
Estaife Lima <estaife@icloud.com>
2021-10-21 17:03:07 -0300

    Fixing SimpleMovieResponse

--- Domain/UseCases/Models/SimpleMovieResponse.swift
+++ Domain/UseCases/Models/SimpleMovieResponse.swift
@@ -12,21 +12,27 @@ public struct SimpleMovieResponse: BaseModel {
     public let releaseDate: String
     public let identifier: Int
     public let title: String
-    public let popularity: Double
+    public let voteAverage: Double
 
     private enum CodingKeys: String, CodingKey {
         case posterPath = "poster_path"
         case releaseDate = "release_date"
         case identifier = "id"
         case title
-        case popularity
+        case voteAverage = "vote_average"
     }
     
-    public init(posterPath: String?, releaseDate: String, identifier: Int, title: String, popularity: Double) {
+    public init(
+        posterPath: String?,
+        releaseDate: String,
+        identifier: Int,
+        title: String,
+        voteAverage: Double
+    ) {
         self.posterPath = posterPath
         self.releaseDate = releaseDate
         self.identifier = identifier
         self.title = title
-        self.popularity = popularity
+        self.voteAverage = voteAverage
     }
 }
